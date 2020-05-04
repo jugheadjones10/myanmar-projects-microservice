ddc1b009deb263c5abac578abb1a80c3aba4e502
Kim Young Jin <kimyoungjin1001@gmail.com>
2020-05-04 22:20:17 +0800

    DD

--- google-maps-part/main.js
+++ google-maps-part/main.js
@@ -25,15 +25,15 @@ map.addControl(
     })
 )
 
-map.on('mousemove', function (e) {
-    document.getElementById('info').innerHTML =
-        // e.point is the x, y coordinates of the mousemove event relative
-        // to the top-left corner of the map
-        JSON.stringify(e.point) +
-        '<br />' +
-        // e.lngLat is the longitude, latitude geographical position of the event
-        JSON.stringify(e.lngLat.wrap())
-})
+// map.on('mousemove', function (e) {
+//     document.getElementById('info').innerHTML =
+//         // e.point is the x, y coordinates of the mousemove event relative
+//         // to the top-left corner of the map
+//         JSON.stringify(e.point) +
+//         '<br />' +
+//         // e.lngLat is the longitude, latitude geographical position of the event
+//         JSON.stringify(e.lngLat.wrap())
+// })
 
 map.on('load', function () {
     // Add a new source from our GeoJSON data and set the
