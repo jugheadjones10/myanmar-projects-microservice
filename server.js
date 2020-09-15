f6e1d30f20dc71ac77ccb5580491abf1bb4c136d
Kim Young Jin <kimyoungjin1001@gmail.com>
2020-09-15 18:34:30 +0800

    AWF

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express")
 var path = require("path")  
 var cors = require('cors')
 const request = require('request')
+var { Project } = require("./models/project")
+
 
 const { locations2, coordsMappings, finalCleaned } = require("./excel-to-mongo/index.js")
 
@@ -22,20 +24,6 @@ mongoose.connect(mongoDB, { useNewUrlParser: true })
 var db = mongoose.connection
 db.on("error", console.error.bind(console, "MongoDB connection error:"))
 
-var { Project } = require("./models/project")
-Project.create({ 
-      _id: new mongoose.mongo.ObjectId(),
-      fullName: "test",
-      category: "test",
-      donationTarget: 1,
-      donationCurrent: 1,
-      locations: [{lat: 12, long: 12}],
-      updates: ["test"],
-      description: "test"
-}, function (err, project_instance) {
-  if (err) console.log("There has been the following error: " + err)
-  console.log(JSON.stringify(project_instance))
-})
 
 //Cors allows webpack dev server at localhost:8080 to access my myanmar map API
 app.use(cors())
@@ -65,10 +53,52 @@ app.get("/coordsMappings", async function(req, res){
 })
 
 app.get("/finalCleaned", async function(req, res){
-  // locations.then(locations => {
-    var finalCleanedFulfilled = await finalCleaned
-    res.send(finalCleanedFulfilled)
-  // })
+  var finalCleanedFulfilled = await finalCleaned
+
+  finalCleanedFulfilled.forEach(x => {
+
+    //Ensures that there is AT LEAST ONE valid lat long pair in the array of locations
+    var filtered = x.Location.filter(y => {
+      if(y.long !== ""){
+        return y
+      }
+    })
+
+    if(filtered.length > 0){
+
+      var locationsArr = x.Location.filter(x => {
+
+        if(x.lat !== ""){
+          return x
+        }
+
+      }).map(x => {
+        return {
+          lat: x.lat,
+          long: x.long
+        }
+      })
+
+      Project.create({ 
+            _id: new mongoose.mongo.ObjectId(),
+            fullName: x["Name of Events"],
+            category: x["Specific Type of Project"],
+            donationTarget: 1,
+            donationCurrent: 1,
+            locations: locationsArr,
+            updates: x["URL Link to Facebook Post"],
+            description: x["Update/ General Description"]
+      }, function (err, project_instance) {
+        if (err) console.log("There has been the following error: " + err)
+        console.log(JSON.stringify(project_instance))
+      })
+
+    }
+ 
+  })
+
+
+  res.send(finalCleanedFulfilled)
 })
 
 
